c254828597e26808216c080f40f4ec692f8cd3bc
Ctuna89 <freamer@gmail.com>
2025-08-24 18:13:53 -0400

    Create __init__.py

---
+++ custom_components/chile_swissknife/__init__.py
@@ -0,0 +1,73 @@
+"""The Chile Swissknife integration."""
+import asyncio
+import logging
+from datetime import timedelta
+
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.update_coordinator import DataUpdateCoordinator
+
+from .const import DOMAIN
+
+_LOGGER = logging.getLogger(__name__)
+
+PLATFORMS = ["sensor"]
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Set up Chile Swissknife from a config entry."""
+    
+    coordinator = ChileSwissknifeDataUpdateCoordinator(
+        hass,
+        entry=entry,
+    )
+    
+    await coordinator.async_config_entry_first_refresh()
+
+    hass.data.setdefault(DOMAIN, {})
+    hass.data[DOMAIN][entry.entry_id] = coordinator
+
+    for platform in PLATFORMS:
+        hass.async_create_task(
+            hass.config_entries.async_forward_entry_setup(entry, platform)
+        )
+
+    return True
+
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Unload a config entry."""
+    unload_ok = all(
+        await asyncio.gather(
+            *[
+                hass.config_entries.async_forward_entry_unload(entry, platform)
+                for platform in PLATFORMS
+            ]
+        )
+    )
+    if unload_ok:
+        hass.data[DOMAIN].pop(entry.entry_id)
+
+    return unload_ok
+
+
+class ChileSwissknifeDataUpdateCoordinator(DataUpdateCoordinator):
+    """Class to manage fetching Chile Swissknife data."""
+
+    def __init__(self, hass, entry):
+        """Initialize global Chile Swissknife data updater."""
+        self.entry = entry
+        self.bus_stops = entry.data.get("bus_stops", [])
+        
+        update_interval = timedelta(minutes=entry.data.get("update_interval", 10))
+        
+        super().__init__(
+            hass,
+            _LOGGER,
+            name=DOMAIN,
+            update_interval=update_interval,
+        )
+
+    async def _async_update_data(self):
+        """Fetch data from Chile Swissknife."""
+        # This will be implemented in sensor.py
+        return {}
